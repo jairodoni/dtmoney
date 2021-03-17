1cc0888add81d1b20f343bbd14ca88341e9b6367
Jairo Doni <jairo.doni97@gmail.com>
2021-03-17 16:26:11 -0300

    Estilização de modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
-import logoImg from '../../assets/logo.svg'
 
+import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionsModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionsModal }:HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money"/>
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionsModal}>
           Nova transação
         </button>
       </Content>
